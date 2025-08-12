ab69de1e9a1b73ee42965d92c4cebffbde8f56e2
Azbabdesignhub <moshaaallah000@gmail.com>
2025-08-12 00:15:08 -0700

    Add new admin

--- data/users.js
+++ data/users.js
@@ -1,37 +1,22 @@
 import bcrypt from 'bcryptjs'
 const Users = [
     {
-        name : 'Abdessamad bourhjoul',
-        email : 'Abdessamadbourhjoul@gmail.com',
-        password : bcrypt.hashSync('samides2020',12),
+        name : 'Babalola Abdulazeez',
+        email : 'moshaaallah000@gmail.com',
+        password : bcrypt.hashSync('developer',12),
         isAdmin : true,
         no : 9013562269,
         isVerified:true
     },
     {
-        name : 'Souhail Ouabou',
-        email : 'Ouabou.souhail@ump.ac.com',
+        name : 'Babalola Ibrahim',
+        email : 'hidressupibadan',
         password : bcrypt.hashSync('souhail2020',12),
-        isAdmin : false,
-        no : 9013562269,
+        isAdmin : true,
+        no : 8165732866,
         isVerified:true
     },
-    {
-        name : 'Soufian Zaam',
-        email : 'Soufian.Zaam@ump.ac.com',
-        password : bcrypt.hashSync('zaam2020',12),
-        isAdmin : false,
-        no : 9013562269,
-        isVerified:true
-    },
-    {
-        name : 'John Doe',
-        email : 'JohnDoe2@gmail.com',
-        password : bcrypt.hashSync('doe2020',12),
-        isAdmin : false,
-        no : 8157808185,
-        isVerified:true
-    }
-    
+
+
 ]
 export default Users
